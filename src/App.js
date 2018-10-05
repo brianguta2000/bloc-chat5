9fc57e4fbcba4889ef8a853c2ea8502a49664af0
brian guta <brianguta2000@yahoo.com>
2018-10-04 19:31:32 -0500

    checkpoint-2-complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import RoomList from './components/RoomList';
 import './App.css';
+import * as firebase from 'firebase';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDWn4jF8gxu7VBDv0OTgYxPJ4cdSB-fGr0",
+    authDomain: "bloc-chat-82224.firebaseapp.com",
+    databaseURL: "https://bloc-chat-82224.firebaseio.com",
+    projectId: "bloc-chat-82224",
+    storageBucket: "bloc-chat-82224.appspot.com",
+    messagingSenderId: "631783572544"
+  };
+  firebase.initializeApp(config);
+
+
+
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <RoomList firebase={firebase} />
+        </div>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
